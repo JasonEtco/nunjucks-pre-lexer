8d5646edd2e63fcfa73f513589aad0c35353f8dc
Jason Etcovitch <jasonetco@github.com>
2018-12-25 11:01:48 -0500

    Comments

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,4 +1,6 @@
 import * as nunjucks from 'nunjucks'
+
+// Extend the Nunjucks typings to define the `parser` object
 declare module 'nunjucks'{
   export const parser: {
     parse(templateString: string): any
