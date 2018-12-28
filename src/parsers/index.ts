593db71645f58d8d2fb8f9bdce67098a07e2afaa
Jason Etcovitch <jasonetco@github.com>
2018-12-28 00:11:33 -0500

    Use LookupVal parser for Set

--- src/parsers/index.ts
+++ src/parsers/index.ts
@@ -5,11 +5,13 @@ import { LookupVal } from './LookupVal'
 interface Parsers {
   FunCall: (schema: object, node: Node, data: any) => any
   LookupVal: (schema: object, node: Node, data: any) => any
+  Set: (schema: object, node: Node, data: any) => any
 }
 
 export type ParserKeys = keyof Parsers
 
 export const parsers: Parsers = {
   FunCall,
-  LookupVal
+  LookupVal,
+  Set: LookupVal // Set and LookupVal use the same logic
 }
